40be9fda47dd242c070e38f7f066accf456fc661
Ceasar Bautista <cbautista2010@gmail.com>
2012-04-20 22:13:49 -0400

    Added program trace.

--- scripts.py
+++ scripts.py
@@ -10,6 +10,18 @@ def generate_debug(filename):
         print '$display("%s: %%b", %s);' % (wire, wire)
 
 
+def pc_trace(filename):
+    with open(filename) as f:
+        for line in f:
+            tokens = line.strip().split(":")
+            if tokens[0] == "m_pc_plus_one":
+                print "PC+1", tokens[1]
+            elif tokens[0] == "m_imem_out":
+                print "INSN", tokens[1]
+
+
 if __name__ == "__main__":
     if sys.argv[1] == 'debug':
         sys.exit(generate_debug(sys.argv[2]))
+    elif sys.argv[1] == 'trace':
+        sys.exit(pc_trace(sys.argv[2]))
